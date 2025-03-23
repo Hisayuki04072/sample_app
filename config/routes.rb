79cec3ad49246841fbd8b23749f5fc7372287eb1
Hisayuki Mutsuda <hk000sy545au-i@outlook.jp>
2025-03-23 09:36:50 +0000

    Add test and html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do #to rails 
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
   root "application#hello"
 end
