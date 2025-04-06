70608fd0219fc86e06267d83312c48838bd89f94
Hisayuki04072 <h.mutsuda.24.3.1284@4nonome.com>
2025-04-06 21:04:43 +0900

    ADD account activation

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do #to rails
   post "/login", to:"sessions#create"
   delete "/logout", to:"sessions#destroy"
   resources :users
+  resources :account_activations, only: [:edit]
 end
