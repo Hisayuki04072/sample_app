d4366392d193110a8ef68fc6bbab19aaaf152578
Hisayuki04072 <h.mutsuda.24.3.1284@4nonome.com>
2025-04-17 15:40:32 +0900

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,15 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: {minimum: 8}, allow_nil: true
   has_many :microposts, dependent: :destroy
-
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+                              
   #渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -63,9 +71,26 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships 
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) 
+                     OR user_id = :user_id", user_id: id).includes(:user, image_attachment: :blob)
   end
   
+
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+
   private
     def downcase_email
       email.downcase!
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,7 +10,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   test "should get home" do
-    get home_path
+    get root_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
